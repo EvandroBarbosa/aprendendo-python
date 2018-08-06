bca285caf92a5a8ab60c9062736caa73cdfede79
Evandro Barbosa <vandomarinho.vm@gmail.com>
2018-08-06 15:27:49 -0300

    Dando Continuidade nos Estudos

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,4 @@
-print("HeLlo Python world, Aprendendo Python")
+print("HeLlo Python world, Aprendendo Python","1 2 3 4")
+
+mensage = "Hello Python Crash course world!"
+print(mensage)
---
+++ simple_mensage.py
@@ -0,0 +1,3 @@
+msg = "Fazendos os exercicios proposto!"
+mg = " Pelo livro de Python!"
+print(msg + mg)
